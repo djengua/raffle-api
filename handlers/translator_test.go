ab1ef2ebb2c1fce4bb8ca99492bfa88c92fd17eb
Enciso <dav_vd@hotmail.com>
2023-12-19 17:46:06 -0600

    fix pipe

--- handlers/translator_test.go
+++ handlers/translator_test.go
@@ -45,7 +45,7 @@ func TestTranslateAPI(t *testing.T) {
 
 		var resp handlers.Resp
 		// Decodes the body of the response into a struct to be asserted
-		json.Unmarshal(rr.Body.Bytes(), &resp)
+		_ = json.Unmarshal(rr.Body.Bytes(), &resp)
 
 		if resp.Language != test.ExpectedLanguage {
 			t.Errorf(`expected language "%s" but received %s`, test.ExpectedLanguage, resp.Language)
